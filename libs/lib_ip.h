23f16567bfcbc3641c8d9f288acc91a78103733c
AndrewGavluk <gavl.andr96@gmail.com>
2020-11-09 14:06:56 -0800

    next commit

--- libs/lib_ip.h
+++ libs/lib_ip.h
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <array>
 #include <string>
 #include <list>
 #include <vector>
@@ -11,6 +12,7 @@ class ip_printer
 {   
     public:
 
+        
         template <typename T>
         using is_stritg = std::is_same<T,   std::string> ;
         
@@ -20,25 +22,29 @@ class ip_printer
         template <typename T>
         using is_vector = std::is_same<T,   std::vector<typename T::value_type, typename T::allocator_type>> ;
        
-        //for string 
+        //for integral 
         template <typename T1>
-        friend typename std::enable_if_t<is_stritg<T1>::value, void> ip_printer1 (T1& data, ip_printer& printer){
-            printer.m_data = std::string{"is string"}; 
-            (void)(data);}
-
-
-        //for vector 
-        template <typename T1>
-        friend typename std::enable_if_t<is_vector<T1>::value, void> ip_printer1 (T1& data, ip_printer& printer) { 
-             printer.m_data = std::string{"is vector"};
-            (void)(data);
+        friend typename std::enable_if_t<std::is_integral<T1>::value, void> loadInfo (T1& data, ip_printer& printer){
+            auto raw_byte = reinterpret_cast<uint8_t*>(&data);
+            std::array<uint8_t, sizeof(T1)> arr{*raw_byte};
+                for(auto &i : arr)
+                    std::cout << static_cast<int>(i) << ".";
+              (void)(printer);
         }
 
-        //for list 
+        //for string 
         template <typename T1>
-        friend typename std::enable_if_t<is_list<T1>::value, void> ip_printer1 (T1& data, ip_printer& printer) { 
-             printer.m_data = std::string{"is vector"};
+        friend typename std::enable_if_t<is_stritg<T1>::value, void> loadInfo (T1& data, ip_printer& printer){
+            printer.m_data = data; 
             (void)(data);
+            printer.print();}
+
+        //for vector and list
+        template <typename T1>
+        friend typename std::enable_if_t<is_vector<T1>::value || is_list<T1>::value, void> loadInfo (T1& data, ip_printer& printer) { 
+             printer.m_data = std::string{"is container"};
+            (void)(data);
+            printer.print();
         }
 
         ip_printer(): m_data{"empty"}{}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include "../libs/lib_ip.h" 
-int main(){
-foo();
 
-	std::string testString ("mySitename.com");
-	ip_printer t_string;
-	ip_printer1(testString, t_string);
+int main(){
+
+	ip_printer test;
+
+	char testChar{-1};
+	loadInfo(testChar, test); // SFINAE 
+
+	short testshort{0};
+	loadInfo(testshort, test); // SFINAE 
+
+	int testInt{2130706433};
+	loadInfo(testInt, test); // SFINAE 
+
+	long testLong{8875824491850138409};
+	loadInfo(testLong, test); // SFINAE 
+
+	std::string testString ("mySitename.com");	
+	loadInfo(testString, test); // SFINAE 
 
 	std::vector<int> testVector{1,2,3,4};
-	ip_printer t_vector;
-	ip_printer1(testVector, t_vector);
+	loadInfo(testVector, test);  // SFINAE 
 
 	std::list<int> testList{1,2,3,4};
-	ip_printer t_list;
-	ip_printer1(testList, t_list);
+	loadInfo(testList, test);  // SFINAE 
 
 return 0;
 }
